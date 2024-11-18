107b224f591ba5c50920b3169e894623ad9f60fa
Chamly <vladskotarenko6227@gmail.com>
2024-11-18 19:37:01 +0200

    add glfw3 library and base window creation for testing purpose

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,8 +1,22 @@
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main() {
 
 	std::cout << "test outpot!\n";
+
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Test window", nullptr, nullptr);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
 
 	return 0;
 }

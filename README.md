eea7dd53da5c8f590e4cae769fea557930830685
Chamly <vladskotarenko6227@gmail.com>
2024-11-19 18:18:44 +0200

    add Vulkan linking and API call testing

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project Setup
+
+To run this project, follow these steps:
+
+1. Install the [VulkanSDK](https://vulkan.lunarg.com/sdk/home).
+2. After installation, you may need to restart your computer to ensure CMake can properly find the package.
--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,4 +1,5 @@
 #include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
 
 #include <iostream>
 
@@ -9,6 +10,11 @@ int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	GLFWwindow* window = glfwCreateWindow(800, 600, "Test window", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	std::cout << extensionCount << " extensions supported\n";
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
